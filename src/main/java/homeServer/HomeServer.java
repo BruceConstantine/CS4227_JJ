fa91169f11053116c588345bec314ebaeb947b47
MeiyuQi <qimeiyu827@outlook.com>
2017-10-26 22:36:12 +0100

    b

--- src/main/java/homeServer/HomeServer.java
+++ src/main/java/homeServer/HomeServer.java
@@ -2,21 +2,21 @@ package homeServer;
 
 public class HomeServer {
 	private Initaliser initaliser;
-	private DatabaseManager databaseManager;
-	private ConfigManager configManager;
-	private DeviceManager deviceManager;
-	private ConnectionManager connectionManager;
+	private DatabaseManager databaseManager = new DatabaseManager();
+	private ConfigManager configManager = new ConfigManager();
+	private DeviceManager deviceManager = new DeviceManager();
+	private ConnectionManager connectionManager = new ConnectionManager();
 	
 	public HomeServer(String configFileName){
 		initalise(configFileName);
-		
-		initaliser.test();
 	}
 	
 	private void initalise(String configFileName) {
 		this.initaliser = new Initaliser(configFileName, databaseManager, configManager, deviceManager, connectionManager);
-//		System.out.println(initaliser.getSummary());
 	}
-
+	
+	public static void main(String[] args) {
+		HomeServer homeServer =  new HomeServer(args[0]);
+	}
 }
   
